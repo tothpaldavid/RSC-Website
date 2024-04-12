4eb94b70c2c8c5a96a0a61097f19bc342d1b5867
David Tothpal <tothpaldavidka@gmail.com>
2024-04-12 22:30:30 +0300

    Változtatások a RSC-Website repository-ba

---
+++ services.js
@@ -0,0 +1,16 @@
+
+document.getElementById('button1').addEventListener('click', function() {
+    document.body.style.backgroundImage = 'url("galery/bender.jpg")';
+  });
+  
+  // Repeat for each button
+  window.onload = function() {
+    document.getElementById('button1').addEventListener('click', function() {
+      document.body.style.backgroundImage = 'url("galery/benderjpg")';
+    });
+  
+    // Repeat for each button
+  };
+
+
+
